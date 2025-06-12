1fc6c9251bbad22c3479744620085141bb090114
Caiouuill <caiototal12@gmail.com>
2025-06-11 21:35:11 -0300

    Implementação do DAO no sistema

--- server.js
+++ server.js
@@ -26,8 +26,44 @@ app.post("/alunos", (req, res) => {
         const novoAluno = Alunos.create(req.body);
         res.json(novoAluno);
     } catch (err) {
-        res.json({err: err});
+        res.json({ err: err });
     }
 });
+
+app.get('/alunos', async (req, res) => {
+    try {
+        const aluno = await Alunos.find()
+        res.json(aluno);
+    } catch (error) {
+        res.json({ error: error });
+    }
+})
+
+app.put('/alunos/:id', async (req, res) => {
+    //req.params.id
+    try {
+        const novoaluno = await Alunos.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new : true }
+        )
+        res.json(novoaluno);
+    } catch (error) {
+        res.json({ error: error });
+    }
+})
+
+
+app.delete('/alunos/:id', async (req, res) => {
+    //req.params.id
+    try {
+        const anulodeletado = await Alunos.findByIdAndDelete(
+            req.params.id
+        )
+        res.json(alunodeletado);
+    } catch (error) {
+        res.json({ error: error });
+    }
+})
 
 app.listen(PORT, () => { console.log(`O servidor está na porta ${PORT}`); })
