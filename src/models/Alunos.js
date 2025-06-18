429b48432651204150c31ae84c76181a14059065
Jaqueline Klegin <18li3ad3@gmail.com>
2025-06-18 16:05:42 -0300

    Adicionando validação e registro das alterações

--- src/models/Alunos.js
+++ src/models/Alunos.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
 const AlunosSchema = new mongoose.Schema({
-    nome: String,
-    idade: Number
-})
+    nome: {type: String, required: [true, "Qual o nome do aluno a ser cadastrado? "]},
+    idade: {type: Number, required: [true, "Qual a idade do aluno? "]},
+    curso: {type: String, required: [true, "Para qual curso o aluno será cadastrado? "]}
+}, {
+    timestamps: true
+});
 
 export default mongoose.model('Aluno', AlunosSchema);
