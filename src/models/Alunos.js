bd937821faf725743ac08313643bff4e4a64cd79
Jaqueline Klegin <18li3ad3@gmail.com>
2025-06-18 18:55:15 -0300

    Retirada da linha curso

--- src/models/Alunos.js
+++ src/models/Alunos.js
@@ -2,8 +2,7 @@ import mongoose from "mongoose";
 
 const AlunosSchema = new mongoose.Schema({
     nome: {type: String, required: [true, "Qual o nome do aluno a ser cadastrado? "]},
-    idade: {type: Number, required: [true, "Qual a idade do aluno? "]},
-    curso: {type: String, required: [true, "Para qual curso o aluno será cadastrado? "]}
+    idade: {type: Number, required: [true, "Qual a idade do aluno? "]}
 }, {
     timestamps: true
 });
